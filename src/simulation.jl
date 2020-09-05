688958b67916daca410c0a560612d4c8faf7f44f
GiggleLiu <cacate0129@gmail.com>
2020-09-04 21:26:41 -0400

    ...

--- src/simulation.jl
+++ src/simulation.jl
@@ -137,180 +137,32 @@ function compute_PML_Params!(param::AcousticPropagatorParams)
     return param
 end
 
-function compute_PML_Params(param::ElasticPropagatorParams)
-    ## define profile of absorption in PML region
-    NPOINTS_PML = param.NPOINTS_PML
-    NPOWER = param.NPOWER
-    NX = param.NX
-    NY = param.NY
-    DELTAX = param.DELTAX
-    DELTAY = param.DELTAY
-    DELTAT = param.DELTAT
-    K_MAX_PML = param.K_MAX_PML
-    ALPHA_MAX_PML = param.ALPHA_MAX_PML
-    USE_PML_XMIN = param.USE_PML_XMIN
-    USE_PML_XMAX = param.USE_PML_XMAX
-    USE_PML_YMIN = param.USE_PML_YMIN
-    USE_PML_YMAX = param.USE_PML_YMAX
-    Rcoef = param.Rcoef
-    
-    thickness_PML_x = NPOINTS_PML * DELTAX
-    thickness_PML_y = NPOINTS_PML * DELTAY
-    
-    # reflection coefficient
-    d0_x = - (NPOWER + 1) * param.vp_ref * log(Rcoef) / (2. * thickness_PML_x)
-    d0_y = - (NPOWER + 1) * param.vp_ref * log(Rcoef) / (2. * thickness_PML_y)
-    
-    xoriginleft = thickness_PML_x
-    xoriginright = (NX-0.5)*DELTAX - thickness_PML_x
-    
-    a_x = zeros(NX)
-    a_y = zeros(NY)
-    a_x_half = zeros(NX)
-    a_y_half = zeros(NY)
-    b_x = zeros(NX)
-    b_y = zeros(NY)
-    b_x_half = zeros(NX)
-    b_y_half = zeros(NY)
-    d_x = zeros(NX)
-    d_y = zeros(NY)
-    d_x_half = zeros(NX)
-    d_y_half = zeros(NY)
-    K_x = ones(NX)
-    K_y = ones(NY)
-    K_x_half = ones(NX)
-    K_y_half = ones(NY)
-    alpha_x = zeros(NX)
-    alpha_y = zeros(NY)
-    alpha_x_half = zeros(NX)
-    alpha_y_half = zeros(NY)
-    
-    for i = 1:NX
-        xval = DELTAX * (i-1)
-        if (USE_PML_XMIN)
-            
-            abscissa_in_PML = xoriginleft - xval
-            if (abscissa_in_PML >= 0.0)
-                abscissa_normalized = abscissa_in_PML / thickness_PML_x
-                d_x[i] = d0_x * abscissa_normalized^NPOWER
-                K_x[i] = 1. + (K_MAX_PML - 1.) * abscissa_normalized^NPOWER
-                # alpha_x[i] = ALPHA_MAX_PML * (1. - abscissa_normalized)
-                alpha_x[i] = ALPHA_MAX_PML * abscissa_normalized
-            end
-            
-            abscissa_in_PML = xoriginleft - (xval + DELTAX/2.)
-            if (abscissa_in_PML >= 0.0)
-                abscissa_normalized = abscissa_in_PML / thickness_PML_x
-                d_x_half[i] = d0_x * abscissa_normalized^NPOWER
-                K_x_half[i] = 1. + (K_MAX_PML - 1.) * abscissa_normalized^NPOWER
-                # alpha_x_half[i] = ALPHA_MAX_PML * (1. - abscissa_normalized)
-                alpha_x_half[i] = ALPHA_MAX_PML * abscissa_normalized
-            end
-            
-        end
-        
-        if (USE_PML_XMAX)
-            abscissa_in_PML = xval - xoriginright
-            if (abscissa_in_PML >= 0)
-                abscissa_normalized = abscissa_in_PML / thickness_PML_x
-                d_x[i] = d0_x * abscissa_normalized^NPOWER
-                K_x[i] = 1. + (K_MAX_PML - 1.) * abscissa_normalized^NPOWER
-                # alpha_x[i] = ALPHA_MAX_PML * (1. - abscissa_normalized)
-                alpha_x[i] = ALPHA_MAX_PML * abscissa_normalized
-            end
-            
-            abscissa_in_PML = xval + DELTAX/2.0 - xoriginright
-            if (abscissa_in_PML >= 0)
-                abscissa_normalized = abscissa_in_PML / thickness_PML_x
-                d_x_half[i] = d0_x * abscissa_normalized^NPOWER
-                K_x_half[i] = 1. + (K_MAX_PML - 1.) * abscissa_normalized^NPOWER
-                # alpha_x_half[i] = ALPHA_MAX_PML * (1. - abscissa_normalized)
-                alpha_x_half[i] = ALPHA_MAX_PML * abscissa_normalized
-            end
-        end
-        
-        if (alpha_x[i] < 0) 
-            alpha_x[i] = 0
-        end
-        if (alpha_x_half[i] < 0) 
-            alpha_x_half[i] = 0
-        end
-        
-        b_x[i] = exp(- (d_x[i] / K_x[i] + alpha_x[i]) * DELTAT)
-        b_x_half[i] = exp(- (d_x_half[i] / K_x_half[i] + alpha_x_half[i]) * DELTAT)
-        
-        if (abs(d_x[i]) > 1e-6) 
-            a_x[i] = d_x[i] * (b_x[i] - 1.) / (K_x[i] * (d_x[i] + K_x[i] * alpha_x[i]))
-        end
-        if (abs(d_x_half[i]) > 1e-6) 
-            a_x_half[i] = d_x_half[i] * (b_x_half[i] - 1.) / (K_x_half[i] * (d_x_half[i] + K_x_half[i] * alpha_x_half[i]))
-        end
-    end
-    
-    yoriginbottom = thickness_PML_y
-    yorigintop = (NY-0.5)*DELTAY - thickness_PML_y
-    
-    for j = 1:NY
-        
-        yval = DELTAY * (j-1)
-        if param.USE_PML_YMIN
-            abscissa_in_PML = yoriginbottom - yval
-            if (abscissa_in_PML >= 0.0)
-                abscissa_normalized = abscissa_in_PML / thickness_PML_y
-                d_y[j] = d0_y * abscissa_normalized^NPOWER
-                K_y[j] = 1. + (K_MAX_PML - 1.) * abscissa_normalized^NPOWER
-                # alpha_y[j] = ALPHA_MAX_PML * (1. - abscissa_normalized)
-                alpha_y[j] = ALPHA_MAX_PML * abscissa_normalized
-            end
-            abscissa_in_PML = yoriginbottom - (yval + DELTAY/2.)
-            if abscissa_in_PML >= 0
-                abscissa_normalized = abscissa_in_PML / thickness_PML_y
-                d_y_half[j] = d0_y * abscissa_normalized^NPOWER
-                K_y_half[j] = 1. + (K_MAX_PML - 1.) * abscissa_normalized^NPOWER
-                # alpha_y_half[j] = ALPHA_MAX_PML * (1. - abscissa_normalized)
-                alpha_y_half[j] = ALPHA_MAX_PML * abscissa_normalized
-            end
-        end
-        
-        if param.USE_PML_YMAX
-            abscissa_in_PML = yval - yorigintop
-            if abscissa_in_PML >= 0
-                abscissa_normalized = abscissa_in_PML / thickness_PML_y
-                d_y[j] = d0_y * abscissa_normalized^NPOWER
-                K_y[j] = 1. + (K_MAX_PML - 1.) * abscissa_normalized^NPOWER
-                # alpha_y[j] = ALPHA_MAX_PML * (1. - abscissa_normalized)
-                alpha_y[j] = ALPHA_MAX_PML * abscissa_normalized
-            end
-            
-            abscissa_in_PML = yval + DELTAY/2. - yorigintop
-            if abscissa_in_PML >= 0
-                abscissa_normalized = abscissa_in_PML / thickness_PML_y
-                d_y_half[j] = d0_y * abscissa_normalized^NPOWER
-                K_y_half[j] = 1. + (K_MAX_PML - 1.) * abscissa_normalized^NPOWER
-                # alpha_y_half[j] = ALPHA_MAX_PML * (1. - abscissa_normalized)
-                alpha_y_half[j] = ALPHA_MAX_PML * abscissa_normalized
-            end
-        end
-        
-        b_y[j] = exp(- (d_y[j] / K_y[j] + alpha_y[j]) * DELTAT)
-        b_y_half[j] = exp(- (d_y_half[j] / K_y_half[j] + alpha_y_half[j]) * DELTAT)
-        if abs(d_y[j]) > 1e-6
-            a_y[j] = d_y[j] * (b_y[j] - 1.) / (K_y[j] * (d_y[j] + K_y[j] * alpha_y[j]))
-        end
-        if (abs(d_y_half[j]) > 1e-6) 
-            a_y_half[j] = d_y_half[j] * (b_y_half[j] - 1.) / (K_y_half[j] * (d_y_half[j] + K_y_half[j] * alpha_y_half[j]))
-        end
-    end
-    # @show norm(d_x-d_y)
+"""
+    pml_helper(x::Float64, y::Float64, param::AcousticPropagatorParams)
 
-    ax = [a_x'; a_x_half']
-    bx = [b_x'; b_x_half']
-    kx = [K_x'; K_x_half']
-    ay = [a_y'; a_y_half']
-    by = [b_y'; b_y_half']
-    ky = [K_y'; K_y_half']
+Computing the PML profile. 
+"""
+function pml_helper(x::Float64, y::Float64, param::AcousticPropagatorParams)
+    outx = 0.0; outy = 0.0
+    ξx = param.damping_x
+    Lx = param.NPOINTS_PML * param.DELTAX
+    if x<Lx && param.USE_PML_XMIN
+        d = abs(Lx-x)
+        outx = ξx * (d/Lx - sin(2π*d/Lx)/(2π))
+    elseif x>param.DELTAX*(param.NX+1)-Lx && param.USE_PML_XMAX
+        d = abs(x-(param.DELTAX*(param.NX+1)-Lx))
+        outx = ξx * (d/Lx - sin(2π*d/Lx)/(2π))
+    end
+
+    ξy = param.damping_y
+    Ly = param.NPOINTS_PML * param.DELTAY
+    if y<Ly && param.USE_PML_YMIN
+        d = abs(Ly-y)
+        outy = ξy * (d/Ly - sin(2π*d/Ly)/(2π))
+    elseif y>param.DELTAY*(param.NY+1)-Ly && param.USE_PML_YMAX
+        d = abs(y-(param.DELTAY*(param.NY+1)-Ly))
+        outy = ξy * (d/Ly - sin(2π*d/Ly)/(2π))
+    end
     
-    
-    return ax,bx,kx,ay,by,ky
-    
+    return outx, outy
 end
